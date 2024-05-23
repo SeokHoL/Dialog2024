4608112f147bbf13a036903a14306e51d83b368a
SeokHoL <sukho8407@gmail.com>
2024-05-23 16:25:44 +0900

    체크박스 목록 대화상자

--- app/src/main/java/kr/ac/kopo/dialog2024/MainActivity.kt
+++ app/src/main/java/kr/ac/kopo/dialog2024/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
 
         var btnDialog =findViewById<Button>(R.id.btnDialog)
         
-        //라디오 버튼 목록 대화상자
+        //체크박스 버튼 목록 대화상자
         btnDialog.setOnClickListener{
             var placesArr = arrayOf("프라하","카파도키아","포르투")
             var selectArr = booleanArrayOf(false, true, false)
@@ -22,8 +22,8 @@ class MainActivity : AppCompatActivity() {
             dialog.setIcon(R.drawable.conversation)
             //d1: 클릭된 대화 상자를 나타내는 DialogInterface 객체.
             //which: 클릭된 항목의 인덱스를 나타내는 Int.
-            dialog.setSingleChoiceItems(placesArr, 1){ d1, which ->
-                btnDialog.text = placesArr[which]
+            dialog.setMultiChoiceItems(placesArr, selectArr){ d1, which, isChecked ->
+                btnDialog.text =placesArr[which]
 
             }
             dialog.setPositiveButton("닫기", null)
